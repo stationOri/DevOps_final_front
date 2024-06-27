edf3f80cf42421529309173cb8109ce33c3097ed
myungji2 <myungji129@naver.com>
2024-06-27 09:53:51 +0900

    first commit

---
+++ src/main/java/com/example/github_actions_test_03/GithubActionsTest03Application.java
@@ -0,0 +1,13 @@
+package com.example.github_actions_test_03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsTest03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsTest03Application.class, args);
+	}
+
+}
