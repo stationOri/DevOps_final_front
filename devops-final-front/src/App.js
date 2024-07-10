0cb38b6fc605c6ad7f7972a7859f0193b2655237
starht <star2thb@gmail.com>
2024-07-10 16:55:34 +0900

    Feat: 식당 상세 페이지

--- devops-final-front/src/App.js
+++ devops-final-front/src/App.js
@@ -4,18 +4,19 @@ import Main from "./pages/Main";
 import Mypage from "./pages/Mypage";
 import RestaurantSignup from "./pages/RestaurantSignup";
 import UserSignup from "./pages/UserSignup";
-
+import RestDetailPage from "./pages/RestDetailPage";
 
 function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main />}></Route>
-          <Route path="/mypage" element={<Mypage />}></Route>
-          <Route path="/restaurantsignup" element={<RestaurantSignup />}></Route>
-          <Route path="/usersignup" element={<UserSignup />}></Route>
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/mypage" element={<Mypage />}></Route>
+        <Route path="/restaurantsignup" element={<RestaurantSignup />}></Route>
+        <Route path="/usersignup" element={<UserSignup />}></Route>
+        <Route path="/restaurants/:id" element={<RestDetailPage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
