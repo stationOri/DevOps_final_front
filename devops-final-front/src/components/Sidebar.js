ee7db09bbf1577d7e5d9479cad9ee16e0e36cdfe
01022549653@naver.com <>
2024-07-11 10:22:45 +0900

    feat: sidebar

--- devops-final-front/src/components/Sidebar.js
+++ devops-final-front/src/components/Sidebar.js
@@ -1,68 +1,103 @@
-import Logo from "../assets/images/oriblue.png"
-import Home from "../assets/images/home.png"
-import Restaurant from "../assets/images/restaurant.png"
-import Reservation from "../assets/images/reservation.png"
-import Chat from "../assets/images/chat.png"
-import Login from "../assets/images/login.png"
-import Search from "../assets/images/search.png"
-import ExtendBtn from "../assets/images/menubtn.png"
-import "../css/components/Sidebar.css"
+import React, { useState } from 'react';
+import Logo from "../assets/images/oriblue.png";
+import Home from "../assets/images/home.png";
+import Restaurant from "../assets/images/restaurant.png";
+import Reservation from "../assets/images/reservation.png";
+import Chat from "../assets/images/chat.png";
+import Login from "../assets/images/login.png";
+import Search from "../assets/images/search.png";
+import ExtendBtn from "../assets/images/menubtn.png";
+import "../css/components/SideBar.css";
 
 function SideBar() {
-  
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState("Guest");
+  const [isExtended, setIsExtended] = useState(true); // 사이드바 확장 상태를 관리하는 변수
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+    setUsername("User Name"); // 실제 사용자 이름 가져오기
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setUsername("Guest");
+  };
+
+  const toggleSidebar = () => {
+    setIsExtended(!isExtended); // 사이드바 확장 상태 토글
+  };
+
   return (
-    <div className="sideBarWrapper">
+    <div className={`sideBarWrapper ${isExtended ? 'extended' : 'collapsed'}`}>
       <div className="sideBarHeader">
         <div className="iconWrapper">
           <img src={Logo} alt="" className="sidebarLogo"/>
-          <div className="guestText">Guest</div>
+          {isExtended && <div className="guestText">{username}</div>}
         </div>
-        <div className="sidebarsearchboxWrapper">
-          <img src={Search} alt="" className="searchLogo"/>
-          <input 
-            type="text" 
-            className="sidebarsearchbox"
-            placeholder="Search"
+        <div className={`sidebarsearchboxWrapper ${isExtended ? '' : 'hidden'}`}>
+            <img src={Search} alt="" className="searchLogo"/>
+        {isExtended && (
+            <input 
+              type="text" 
+              className="sidebarsearchbox"
+              placeholder="Search"
             ></input>
-        </div>
+          )}
+          </div>
       </div>
-      <button className="extendbtn">
-          <img src={ExtendBtn} alt="" className="extendbtnImg"/>
+      <button className="extendbtn" onClick={toggleSidebar}>
+        <img src={ExtendBtn} alt="" className="extendbtnImg"/>
       </button>
       <div className="sidebarContent">
         <div className="mainContent">
           <div className="sidebarRow">
-          <div className="ctgText">MAIN</div>
+            <div className={`ctgText ${isExtended ? '' : 'hidden'}`}>MAIN</div>
           </div>
           <div className="sidebarRow">
             <img src={Home} alt="" className="sidebarIcon"/>
-            <div className="sidebarText">홈</div>
+            <div className={`sidebarText ${isExtended ? '' : 'hidden'}`}>홈</div>
           </div>
           <div className="sidebarRow">
             <img src={Restaurant} alt="" className="sidebarIcon rest"/>
-            <div className="sidebarText">식당 조회</div>
+            <div className={`sidebarText ${isExtended ? '' : 'hidden'}`}>식당 조회</div>
           </div>
           <div className="sidebarRow">
             <img src={Reservation} alt="" className="sidebarIcon"/>
-            <div className="sidebarText">예약</div>
+            <div className={`sidebarText ${isExtended ? '' : 'hidden'}`}>예약</div>
           </div>
           <div className="sidebarRow">
             <img src={Chat} alt="" className="sidebarIcon"/>
-            <div className="sidebarText">1:1 문의</div>
+            <div className={`sidebarText ${isExtended ? '' : 'hidden'}`}>1:1 문의</div>
           </div>
         </div>
         <div className="userContent">
-        <div className="sidebarRow">
-          <div className="ctgText">USER</div>
-          </div>
           <div className="sidebarRow">
-            <img src={Login} alt="" className="sidebarIcon"/>
-            <div className="sidebarText">로그인</div>
+            <div className={`ctgText ${isExtended ? '' : 'hidden'}`}>USER</div>
           </div>
-          <div className="sidebarRow">
-            <img src={Login} alt="" className="sidebarIcon"/>
-            <div className="sidebarText">회원가입</div>
-          </div>
+          {isLoggedIn ? (
+            <>
+              <div className="sidebarRow">
+                <img src={Login} alt="" className="sidebarIcon"/>
+                <div className={`sidebarText ${isExtended ? '' : 'hidden'}`}>마이페이지</div>
+              </div>
+              <div className="sidebarRow">
+                <img src={Login} alt="" className="sidebarIcon"/>
+                <div className={`sidebarText ${isExtended ? '' : 'hidden'}`} onClick={handleLogout}>로그아웃</div>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="sidebarRow">
+                <img src={Login} alt="" className="sidebarIcon"/>
+                <div className={`sidebarText ${isExtended ? '' : 'hidden'}`} onClick={handleLogin}>로그인</div>
+              </div>
+              <div className="sidebarRow">
+                <img src={Login} alt="" className="sidebarIcon"/>
+                <div className={`sidebarText ${isExtended ? '' : 'hidden'}`}>회원가입</div>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
