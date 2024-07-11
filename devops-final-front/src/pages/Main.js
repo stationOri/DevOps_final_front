0d230e4bbcb8c4bbf030fdf561f56e2eb938f9c8
01022549653@naver.com <>
2024-07-11 18:11:58 +0900

    Feat: main loading 적용 및 캐러셀 컴포넌트 연결

--- devops-final-front/src/pages/Main.js
+++ devops-final-front/src/pages/Main.js
@@ -1,24 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import HeaderOrange from "../components/HeaderOrange";
 import SideBar from "../components/SideBar";
+import Loading from "../components/Loading";
 import "../css/pages/Main.css";
-
+import Carousel from '../components/Carousel';
 
 function Main() {
   const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
+  const [bannerfoods, setBannerFoods] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const toggleSidebar = () => {
-    setIsSidebarCollapsed(!isSidebarCollapsed);
+  const getBannerfood = async () => {
+    try {
+      const response = await fetch(`http://localhost:4000/restaurant`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch");
+      }
+      const json = await response.json();
+      console.log('Fetched data:', json);
+      setBannerFoods(json || []); // 여기서 json.row 가 아니라 그냥 json을 사용합니다.
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      setLoading(false);
+    }
   };
 
-  return (
-    <div className={`mainWrapper ${isSidebarCollapsed ? 'collapsed' : ''}`}>
-      <SideBar className="mainsidebar" />
-      <div className={`contentsWrapper ${isSidebarCollapsed ? 'collapsed' : ''}`}>
-        <HeaderOrange />
-      </div>
-    </div>
+  useEffect(() => {
+    getBannerfood();
+  }, []);
 
+  return (
+    <div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className={`mainWrapper ${isSidebarCollapsed ? 'collapsed' : ''}`}>
+          <SideBar className="mainsidebar" />
+          <div className={`contentsWrapper ${isSidebarCollapsed ? 'collapsed' : ''}`}>
+            <HeaderOrange />
+            <div>
+              {bannerfoods.map(rest => (
+                <div key={rest.id}>
+                  <Carousel 
+                    rest_name={rest.rest_name}
+                    rest_photo={rest.rest_photo}
+                    rest_grade={rest.rest_grade}
+                    rest_address={rest.rest_address}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
 
