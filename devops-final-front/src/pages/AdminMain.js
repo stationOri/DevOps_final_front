3e39dbbc18f6f60c1e79c6aa327fcb688c79d7ef
01022549653@naver.com <>
2024-07-11 16:39:27 +0900

    Feat: admin sidebar

--- devops-final-front/src/pages/AdminMain.js
+++ devops-final-front/src/pages/AdminMain.js
@@ -1,4 +1,5 @@
 import AdminNav from "../components/AdminNav";
+import AdminSideBar from "../components/AdminSideBar";
 import HeaderBlue from "../components/HeaderBlue"
 import "../css/pages/AdminMain.css"
 
@@ -7,8 +8,16 @@ function AdminMain() {
   
   return (
     <div className="adminMainWrapper">
+      <div>
       <HeaderBlue />
       <AdminNav />
+      </div>
+      <div className="adminmaincontent">
+        <AdminSideBar className="adsidebar"/>
+        <div className="adminrealcontent">
+          dddd
+        </div>
+      </div>
     </div>
   );
 }
