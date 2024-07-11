2848994bf417b14ad1036f02ddc07884c6c88b8b
01022549653@naver.com <>
2024-07-11 10:23:52 +0900

    feat:main->sidebar+header

--- devops-final-front/src/pages/Main.js
+++ devops-final-front/src/pages/Main.js
@@ -1,11 +1,21 @@
-import SideBar from "../components/Sidebar"
-import "../css/pages/Main.css"
+import React, { useState } from 'react';
+import HeaderOrange from "../components/HeaderOrange";
+import SideBar from "../components/Sidebar";
+import "../css/pages/Main.css";
 
 function Main() {
-  
+  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarCollapsed(!isSidebarCollapsed);
+  };
+
   return (
-    <div>
-      <SideBar className="mainsidebar"/>
+    <div className={`mainWrapper ${isSidebarCollapsed ? 'collapsed' : ''}`}>
+      <SideBar className="mainsidebar" />
+      <div className={`contentsWrapper ${isSidebarCollapsed ? 'collapsed' : ''}`}>
+        <HeaderOrange />
+      </div>
     </div>
   );
 }
