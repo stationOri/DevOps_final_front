03715a5138adb32cd9df8621b2a6591727118db6
01022549653@naver.com <>
2024-07-13 15:28:56 +0900

    feat: 관리자 pagination 화살표 추가

--- devops-final-front/src/components/Pagination.js
+++ devops-final-front/src/components/Pagination.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import "../css/components/Pagination.css";
+import LeftPage from "../assets/images/left-chevron.png";
 
 function Pagination({ totalItems, itemsPerPage, currentPage, onPageChange }) {
   const totalPages = Math.ceil(totalItems / itemsPerPage);
-  
+
   return (
     <div className="paginationWrapper">
+      <button
+        onClick={() => currentPage > 1 && onPageChange(currentPage - 1)}
+        className="paginationButton"
+        disabled={currentPage === 1}
+      >
+        <img src={LeftPage} alt="Previous" className="paginationIcon" />
+      </button>
       {Array.from({ length: totalPages }, (_, index) => (
         <button
           key={index + 1}
@@ -15,6 +23,13 @@ function Pagination({ totalItems, itemsPerPage, currentPage, onPageChange }) {
           {index + 1}
         </button>
       ))}
+      <button
+        onClick={() => currentPage < totalPages && onPageChange(currentPage + 1)}
+        className="paginationButton"
+        disabled={currentPage === totalPages}
+      >
+        <img src={LeftPage} alt="Next" className="paginationIcon forrightarrow" />
+      </button>
     </div>
   );
 }
