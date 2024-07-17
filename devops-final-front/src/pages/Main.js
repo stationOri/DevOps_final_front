90434f543961e8dc3563d813ce1c013ab882186d
01022549653@naver.com <>
2024-07-17 15:17:00 +0900

    Feat: 메인 핫 트렌딩

--- devops-final-front/src/pages/Main.js
+++ devops-final-front/src/pages/Main.js
@@ -1,14 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import HeaderOrange from "../components/HeaderOrange";
-import SideBar from '../components/user/SideBar';
+import SideBar from "../components/user/SideBar";
 import Loading from "../components/Loading";
 import "../css/pages/Main.css";
-import Carousel from '../components/user/Carousel';
+import Carousel from "../components/user/Carousel";
+import RestaurantLocationMap from "../components/RestaurantLocationMap";
+import StarRatingsCarousel from "../components/user/StarRatingCarousel";
 
 function Main() {
-  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
   const [bannerFoods, setBannerFoods] = useState([]);
+  const [trendingFoods, setTrendingFoods] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [isExtended, setIsExtended] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsExtended(!isExtended);
+  };
 
   const getBannerFood = async () => {
     try {
@@ -17,17 +24,32 @@ function Main() {
         throw new Error("Failed to fetch");
       }
       const json = await response.json();
-      console.log('Fetched data:', json);
+      console.log("Fetched banner foods:", json);
       setBannerFoods(json || []);
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching banner foods:", error);
+    }
+  };
+
+  const getTrendingFood = async () => {
+    try {
+      const response = await fetch(`http://localhost:4000/hot`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch");
+      }
+      const json = await response.json();
+      console.log("Fetched trending foods:", json);
+      setTrendingFoods(json || []);
+    } catch (error) {
+      console.error("Error fetching trending foods:", error);
     } finally {
-      setLoading(false);
+      setLoading(false); // 모든 데이터 로딩 완료 시 loading 상태를 false로 설정
     }
   };
 
   useEffect(() => {
     getBannerFood();
+    getTrendingFood();
   }, []);
 
   return (
@@ -35,12 +57,75 @@ function Main() {
       {loading ? (
         <Loading />
       ) : (
-        <div className={`mainWrapper ${isSidebarCollapsed ? 'collapsed' : ''}`}>
-          <SideBar className="mainSidebar" />
-          <div className={`contentsWrapper ${isSidebarCollapsed ? 'collapsed' : ''}`}>
-            <HeaderOrange />
-            <div className="carouselContainer">
-              <Carousel bannerFoods={bannerFoods} />
+        <div className="usermainWrapper">
+          <SideBar className="mainSidebar" toggleSidebar={toggleSidebar} isExtended={isExtended}/>
+          <div className="maincontentsWrapper">
+            <div
+              className={`behindsidebar ${isExtended ? "" : "collapsed"}`}
+            />
+            <div
+              className={`innercontentsWrapper ${isExtended ? "" : "collapsed"}`}
+            >
+              <HeaderOrange />
+              <div className="carouselContainer">
+                <Carousel bannerFoods={bannerFoods} />
+              </div>
+              <div className="mainMapWrapper">
+                <div className="mainRestDetailWrapper">
+                  <img alt="" />
+                  <div className="maincardrestname"></div>
+                  <div className="maincardKeywordWrapper">
+                    <div>keywords</div>
+                  </div>
+                  <div className="mainreviewCardWrapper">
+                    {/* <ReviewCardMain /> */}
+                  </div>
+                </div>
+                <RestaurantLocationMap />
+              </div>
+              <div className="hotTrendingRestWrapper">
+                <div className="hotTrendingHeader">🔥핫 트렌딩 식당🔥</div>
+                {trendingFoods.map((rest, index) => (
+                  <div key={index} className="rankingWrapper">
+                    <div className="rankingLeft">
+                      <div
+                        className="RankingIndex"
+                        style={{
+                          color: index === 0 ? "#FF8A00" : "black",
+                          fontWeight: index === 0 ? "bold" : "",
+                        }}
+                      >
+                        {index + 1}
+                      </div>
+                      <img className="rankPhoto" src={rest.rest_photo} alt="" />
+                      <div className="MainRestNameWrapper">
+                        <div className="MainRestName">{rest.rest_name}</div>
+                        <div className="MainKeywordWrapperMini">
+                          {rest.keyword1 && (
+                            <div className="innerKeywords">
+                              #{rest.keyword1}
+                            </div>
+                          )}
+                          {rest.keyword2 && (
+                            <div className="innerKeywords">
+                              #{rest.keyword2}
+                            </div>
+                          )}
+                          {rest.keyword3 && (
+                            <div className="innerKeywords">
+                              #{rest.keyword3}
+                            </div>
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                    <div className="rankingRight">
+                      <div className="starRatingText">{rest.rest_grade}/5</div>
+                      <StarRatingsCarousel rating={rest.rest_grade} />
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
