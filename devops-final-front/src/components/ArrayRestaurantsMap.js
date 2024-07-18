85e62343dee371646fddd99cc10dc14d4705ef49
01022549653@naver.com <>
2024-07-18 10:17:46 +0900

    Feat: 메인 지도 클릭 시 식당 상세 카드

--- devops-final-front/src/components/ArrayRestaurantsMap.js
+++ devops-final-front/src/components/ArrayRestaurantsMap.js
@@ -4,15 +4,15 @@ import "leaflet/dist/leaflet.css";
 import axios from "axios";
 import markerIcon from "../assets/images/marker.png";
 
-const ArrayRestaurantsMap = ({ addresses }) => {
+const ArrayRestaurantsMap = ({ restaurants, onMarkerClick }) => {
   const mapRef = useRef(null);
-  const mapInstance = useRef(null); // 지도 객체를 저장할 useRef
+  const mapInstance = useRef(null);
+  const markerRef = useRef(null);
 
   useEffect(() => {
     const initializeMap = async () => {
-      if (!mapRef.current || !addresses || addresses.length === 0) return;
+      if (!mapRef.current || !restaurants || restaurants.length === 0) return;
 
-      // 이미 지도 객체가 있으면 기존 객체를 사용하고, 없으면 새로 생성
       if (!mapInstance.current) {
         const defaultLatLng = [37.339832, 127.108985];
         const defaultZoom = 12;
@@ -24,7 +24,7 @@ const ArrayRestaurantsMap = ({ addresses }) => {
           maxZoom: 18,
         }).addTo(map);
 
-        mapInstance.current = map; // useRef에 지도 객체 저장
+        mapInstance.current = map;
       }
 
       const customIcon = L.icon({
@@ -33,15 +33,6 @@ const ArrayRestaurantsMap = ({ addresses }) => {
         iconAnchor: [16, 32],
         popupAnchor: [0, -32],
       });
-
-      const addMarkers = async () => {
-        for (let address of addresses) {
-          const latlng = await getLatLngFromAddress(address);
-          if (latlng) {
-            L.marker(latlng, { icon: customIcon }).addTo(mapInstance.current).bindPopup(address);
-          }
-        }
-      };
 
       const getLatLngFromAddress = async (address) => {
         const apiUrl = "/api/req/address";
@@ -59,18 +50,83 @@ const ArrayRestaurantsMap = ({ addresses }) => {
           const response = await axios.get(apiUrl, { params: params });
           const { result } = response.data.response;
 
-          const { x, y } = result.point;
-          return L.latLng(parseFloat(y), parseFloat(x));
+          if (result && result.point) {
+            const { x, y } = result.point;
+            return L.latLng(parseFloat(y), parseFloat(x));
+          } else {
+            console.error(`주소에서 좌표를 가져오는 중 오류: ${JSON.stringify(result)}`);
+            return null;
+          }
         } catch (error) {
-          console.error("API 호출 오류:", error);
+          console.error(`API 호출 오류 (${address}):`, error);
           return null;
         }
+      };
+
+      const addMarkers = async () => {
+        const markerPromises = restaurants.map(async (restaurant) => {
+          const latlng = await getLatLngFromAddress(restaurant.rest_address);
+          if (latlng) {
+            const marker = L.marker(latlng, { icon: customIcon })
+              .addTo(mapInstance.current)
+              .bindPopup(restaurant.rest_name);
+
+            marker.on("click", () => {
+              if (markerRef.current) {
+                markerRef.current.closePopup();
+              }
+              marker.openPopup();
+              
+              mapRef.current.style.width = "50%";
+              // 클릭 시 마커 위치로 지도 이동
+              mapInstance.current.setView(marker.getLatLng());
+              
+              // 클릭한 마커 참조 저장
+              markerRef.current = marker;
+              onMarkerClick(restaurant);
+            });
+
+            marker.on("popupclose", () => {
+              // 팝업이 닫힐 때 다시 기본 너비로 변경
+              mapRef.current.style.width = "100%";
+              markerRef.current = null;
+
+              onMarkerClick(null);
+            });
+          }
+        });
+
+        await Promise.all(markerPromises);
+
+        if (restaurants.length > 0) {
+          const bounds = await calculateBounds(restaurants);
+          if (bounds.isValid()) {
+            mapInstance.current.fitBounds(bounds);
+          } else {
+            console.warn("유효하지 않은 경계 값:", bounds);
+          }
+        }
+      };
+
+      const calculateBounds = async (restaurants) => {
+        const bounds = new L.LatLngBounds();
+        const latlngPromises = restaurants.map((restaurant) =>
+          getLatLngFromAddress(restaurant.rest_address)
+        );
+        const latlngs = await Promise.all(latlngPromises);
+
+        latlngs.forEach((latlng) => {
+          if (latlng) {
+            bounds.extend(latlng);
+          }
+        });
+
+        return bounds;
       };
 
       addMarkers();
 
       return () => {
-        // 컴포넌트 언마운트 시 지도 객체 제거
         if (mapInstance.current) {
           mapInstance.current.remove();
           mapInstance.current = null;
@@ -79,7 +135,7 @@ const ArrayRestaurantsMap = ({ addresses }) => {
     };
 
     initializeMap();
-  }, [addresses]);
+  }, [restaurants, onMarkerClick]);
 
   return (
     <div
