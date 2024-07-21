293ecc505990090fdb44d8cdbf82a71fec64ae0f
01022549653@naver.com <>
2024-07-21 19:47:43 +0900

    Feat: input -> textarea

--- devops-final-front/src/components/Modal/InputModal.js
+++ devops-final-front/src/components/Modal/InputModal.js
@@ -84,7 +84,7 @@ function InputModal() {
             <div className="InputhintText">신고 내용을 자세하게 작성해주세요.</div>
             <div className='InputModalWrapper'>
               <div className='inputhinttext'>신고내용</div>
-              <input type='text' placeholder='최대 200자 까지 작성 가능합니다.' maxLength={200} className='reportinput'/>
+              <textarea type='text' placeholder='최대 200자 까지 작성 가능합니다.' maxLength={200} className='reportinput'/>
             </div>
             <div className='inputmodalbuttonWrapper'>
               <button className="InputModalreport" onClick={handleReport}>신고</button>
