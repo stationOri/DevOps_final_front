ba6928e4e4c8d9fea557387ed3e31172fb518036
01022549653@naver.com <>
2024-07-23 21:26:28 +0900

    Feat: 키워트 컴포넌트

---
+++ devops-final-front/src/components/restaurant/Keywords.js
@@ -0,0 +1,66 @@
+import Select from "react-select";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function Keywords() {
+  const [allKeywords, setAllKeywords] = useState([]);
+  const [selectedKeyword, setSelectedKeyword] = useState(null);
+
+  useEffect(() => {
+    const fetchKeywords = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/keywords");
+        const keywords = response.data;
+        const formattedKeywords = keywords.map((keyword) => ({
+          value: keyword.keywordId,
+          label: keyword.keyword,
+        }));
+
+        setAllKeywords(formattedKeywords);
+      } catch (error) {
+        console.error("키워드 가져오기 오류:", error);
+      }
+    };
+    fetchKeywords();
+  }, []);
+
+  const handleKeywordChange = (selectedOption) => {
+    setSelectedKeyword(selectedOption);
+  };
+
+  const handleSearch = () => {
+    if (selectedKeyword) {
+      console.log("선택된 키워드:", selectedKeyword.value);
+      // 검색 로직 추가
+    }
+  };
+
+  return(
+    <div className="keywordWrapper">
+              <div className="keyWrapperUpper">
+                <div className="Wrppaerforinput">
+                  <div className="accinfo-hintText">키워드 설정</div>
+                  <Select
+                    options={allKeywords}
+                    value={selectedKeyword}
+                    onChange={handleKeywordChange}
+                    placeholder="키워드 검색/등록"
+                    className="select-box-keyword"
+                    classNamePrefix="react-select"
+                  />
+                </div>
+                <button className="accinfo-button" onClick={handleSearch}>
+                  키워드 등록
+                </button>
+              </div>
+              <div className="keyword">
+                {/* 선택된 키워드를 보여줄 수 있는 부분 */}
+                {selectedKeyword && (
+                  <div>선택된 키워드: {selectedKeyword.label}</div>
+                )}
+              </div>
+            </div>
+  );
+}
+
+export default Keywords;
