03e52082623ea18cbf04f9ffe5cfee898e036a69
starht <star2thb@gmail.com>
2024-07-23 10:34:06 +0900

    Feat: 채팅 등록

--- devops-final-front/src/components/chatt/Chatlist.js
+++ devops-final-front/src/components/chatt/Chatlist.js
@@ -7,22 +7,6 @@ function ChatList({ userId, onChatSelect }) {
   const [chatLists, setChatLists] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const chats = [
-    {
-      id: 1,
-      name: "Liam Anderson",
-      lastMessage: "Hey, how's it going?",
-      time: "04:50 PM",
-    },
-    {
-      id: 2,
-      name: "Lucas Williams",
-      lastMessage: "Hey, how's it going?",
-      time: "10:30 AM",
-    },
-    // ... 더 많은 채팅 목록
-  ];
-
   const getChatList = async () => {
     try {
       const response = await fetch(`http://localhost:8080/chat/user/${userId}`);
