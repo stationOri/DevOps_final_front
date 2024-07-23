7722912f2307f6ce417432f6463b2530c78028a3
01022549653@naver.com <>
2024-07-23 21:26:06 +0900

    Feat: 식당 회원가입

--- devops-final-front/src/pages/RestaurantSignup.js
+++ devops-final-front/src/pages/RestaurantSignup.js
@@ -1,28 +1,27 @@
-import "../css/pages/RestaurantSignup.css"
-import Oriblue from "../assets/images/oriblue.png"
-import File from "../components/File"
-import HeaderOrange from "../components/HeaderOrange"
-import {jwtDecode} from "jwt-decode";
+import "../css/pages/RestaurantSignup.css";
+import Oriblue from "../assets/images/oriblue.png";
+import File from "../components/File";
+import HeaderOrange from "../components/HeaderOrange";
+import { jwtDecode } from "jwt-decode";
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function RestaurantSignin() {
   const [email, setEmail] = useState('');
   const [restName, setRestName] = useState('');
   const [restPhone, setRestPhone] = useState('');
-  const [restName2,setRestName2]=useState('');
-  const [restData,setRestData]=useState(null);
-  const [restImage,setRestImage]=useState(null);
-  const navigate = useNavigate(); 
+  const [restName2, setRestName2] = useState('');
+  const [restData, setRestData] = useState('');
+  const [restImage, setRestImage] = useState(null);
+  const navigate = useNavigate();
 
   function onChangeRestData(value) {
-   
-    const onlyNumber = value.replace(/[^0-9]/g, '')
-    setRestData(onlyNumber)
+    const onlyNumber = value.replace(/[^0-9]/g, '');
+    setRestData(onlyNumber);
   }
+
   useEffect(() => {
-    // 토큰을 localStorage에서 가져오기
     const storedToken = localStorage.getItem('token');
     if (storedToken) {
       try {
@@ -35,18 +34,17 @@ function RestaurantSignin() {
       }
     }
   }, []);
+
   const validateEmail = (email) => {
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailPattern.test(email);
   };
+
   const handleSignup = () => {
     if (!validateEmail(email)) {
       alert('유효하지 않은 이메일 주소입니다.');
       return;
     }
-    // if(restData.length()!=10){
-    //   alert('사업자등록번호는 10자리 입니다.')
-    // }
     if (!restName2 || !restData) {
       alert('모든 필수 항목을 입력해주세요.');
       return;
@@ -60,175 +58,129 @@ function RestaurantSignin() {
       restData,
       restImage
     };
-    
+
     axios.post('http://localhost:8080/register/restaurant', signupData)
-    .then(response => {
-      if(response.data>0){
-        console.log('Signup success:', response.data);
-        alert('[식당 회원가입 완료] 로그인 해주세요.');
-        navigate("/");
-      }else if(response.data===0){
-        alert('[회원가입 실패] 중복된 핸드폰 번호입니다' );
-      }
-      else{
-        alert('[회원가입 실패] 회원가입에 실패하였습니다.' );
-      }
-   
-    // 성공적으로 회원가입이 처리되었을 경우의 처리
-  })
-  .catch(error => {
-    console.error('Signup failed:', error);
-  });
+      .then(response => {
+        if (response.data > 0) {
+          console.log('Signup success:', response.data);
+          alert('[식당 회원가입 완료] 로그인 해주세요.');
+          navigate("/");
+        } else if (response.data === 0) {
+          alert('[회원가입 실패] 중복된 핸드폰 번호입니다');
+        } else {
+          alert('[회원가입 실패] 회원가입에 실패하였습니다.');
+        }
+      })
+      .catch(error => {
+        console.error('Signup failed:', error);
+      });
+  };
 
+  const handleFileChange = (file) => {
+    setRestImage(file);
+  };
 
-};
-
-  
   return (
     <div className="fullWrapper">
-    <HeaderOrange></HeaderOrange>
-    <div className = "restSigninWrapper">
-      <div className="signinHeader">
-        <img className="ori" src={Oriblue} alt="duck"/>
-        <div className="headertext">기업 회원가입</div>
-        <div className="headerexplain">식당 정보 및 업체 정보를 입력해주시기 바랍니다.</div>
-      </div>
-      <div className="signinContent">
-        <div className="row">
-          <div className="input1btn1"> 
-            <div className="explainText">
-              담당자 이메일*
+      <HeaderOrange />
+      <div className="restSigninWrapper">
+        <div className="signinHeader">
+          <img className="ori" src={Oriblue} alt="duck" />
+          <div className="headertext">기업 회원가입</div>
+          <div className="headerexplain">식당 정보 및 업체 정보를 입력해주시기 바랍니다.</div>
+        </div>
+        <div className="signinContent">
+          <div className="row">
+            <div className="input1btn1">
+              <div className="explainText">
+                담당자 이메일*
+              </div>
+              <div className="buttoninputWrapper">
+                <input
+                  type="text"
+                  className="input1"
+                  placeholder="담당자 이메일"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
             </div>
-            <div className="buttoninputWrapper">
-            <input
-              type="text"
-              className="input1"
-              placeholder="담당자 이메일"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            {/* <button className="emailCheckBtn">
-            이메일 중복 확인
-            </button> */}
+          </div>
+          <div className="row">
+            <div className="wrapper2">
+              <div className="explainText">
+                담당자 이름*
+              </div>
+              <input
+                type="text"
+                className="input2"
+                placeholder="담당자 이름"
+                value={restName}
+                disabled
+              />
+            </div>
+          </div>
+          <div className="row child2">
+            <div className="input2btn2short">
+              <div className="explainText">
+                담당자 휴대폰 번호*
+              </div>
+              <div className="buttoninputWrapper">
+                <input
+                  type="text"
+                  className="input1short"
+                  placeholder="'-'제외"
+                  value={restPhone}
+                  disabled
+                />
+              </div>
+            </div>
+          </div>
+          <div className="row child2">
+            <div className="wrapper2">
+              <div className="explainText">
+                매장 이름*
+              </div>
+              <input
+                type="text"
+                className="input2"
+                placeholder="매장 이름"
+                maxLength={100}
+                onChange={(e) => setRestName2(e.target.value)}
+              />
+            </div>
+            <div className="wrapper2">
+              <div className="explainText">
+                사업자 등록번호*
+              </div>
+              <input
+                type="text"
+                className="input2"
+                placeholder="숫자만 입력가능합니다.(10자리)"
+                maxLength={10}
+                minLength={10}
+                value={restData}
+                onChange={(e) => onChangeRestData(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="filebox">
+              <div className="explainText">
+                사업자등록증*
+              </div>
+              <div className="filecompoWrapper">
+                <File onFileChange={handleFileChange} />
+              </div>
             </div>
           </div>
         </div>
-        {/* <div className="row child2">
-          <div className="wrapper2"> 
-            <div className="explainText">
-              비밀번호*
-            </div>
-            <input
-              type="password"
-              className="input2"
-              placeholder="비밀번호"
-            />
-          </div>
-          <div className="wrapper2"> 
-            <div className="explainText">
-              비밀번호 확인*
-            </div>
-            <input
-              type="password"
-              className="input2"
-              placeholder="비밀번호 확인"
-            />
-          </div>
-        </div> */}
-        <div className="row">
-          <div className="wrapper2"> 
-            <div className="explainText">
-              담당자 이름*
-            </div>
-            <input
-              type="text"
-              className="input2"
-              placeholder="담당자 이름"
-              value={restName}
-              disabled
-            />
-          </div>
-        </div>
-        <div className="row child2">
-          <div className="input2btn2short"> 
-            <div className="explainText">
-              담당자 휴대폰 번호*
-            </div>
-            <div className="buttoninputWrapper">
-            <input
-              type="text"
-              className="input1short"
-              placeholder="'-'제외"
-              value={restPhone}
-              disabled
-            />
-            {/* <button className="phoneCheck">
-              인증번호 요청
-            </button> */}
-            </div>
-          </div>
-          {/* <div className="input2btn2short"> 
-            <div className="explainText">
-              인증번호*
-            </div>
-            <div className="buttoninputWrapper">
-            <input
-              type="text"
-              className="input1short"
-              placeholder="입력해주세요"
-            />
-            <button className="numberCheck">
-              확인
-            </button>
-            </div>
-          </div> */}
-        </div>
-        <div className="row child2">
-          <div className="wrapper2"> 
-            <div className="explainText">
-              매장 이름*
-            </div>
-            <input
-              type="text"
-              className="input2"
-              placeholder="매장 이름"
-              maxLength={100}
-              onChange={(e) => setRestName2(e.target.value)}
-            />
-          </div>
-          <div className="wrapper2"> 
-            <div className="explainText">
-              사업자 등록번호*
-            </div>
-            <input
-              type="text"
-              className="input2"
-              placeholder="숫자만 입력가능합니다.(10자리)"
-              maxLength={10}
-              minLength={10}
-              value={restData}
-              onChange={(e)=>onChangeRestData(e.target.value)}
-         
-            />
-          </div>
-        </div>
-        <div className="row">
-        <div className="filebox"> 
-            <div className="explainText">
-              사업자등록증*
-            </div>
-            <div className="filecompoWrapper">
-            <File className="filecompo"></File>
-            </div>
-          </div>
+        <div className="signinFooter">
+          <button className="signinSubmitBtn" onClick={handleSignup}>
+            기업 회원가입
+          </button>
         </div>
       </div>
-      <div className="signinFooter">
-        <button className="signinSubmitBtn" onClick={handleSignup}>
-          기업 회원가입
-        </button>
-      </div>
-    </div>
     </div>
   );
 }
