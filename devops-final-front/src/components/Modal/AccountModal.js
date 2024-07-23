f54f68774300b418eaba9bcb3f73b664c94cf6ba
01022549653@naver.com <>
2024-07-23 13:52:20 +0900

    Feat: account modal 초기화

--- devops-final-front/src/components/Modal/AccountModal.js
+++ devops-final-front/src/components/Modal/AccountModal.js
@@ -14,6 +14,7 @@ function AccountModal({ closeInfoModal, infoshow, restId }) {
     } catch (error) {
       console.error('Error:', error);
     } finally {
+      setAccountNumber("")
       closeInfoModal();
     }
   };
