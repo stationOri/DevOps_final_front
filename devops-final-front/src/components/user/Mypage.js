1dc7dea20e7bd3d877f4990f147638583a2de858
starht <star2thb@gmail.com>
2024-07-23 10:19:47 +0900

    Feat: 웨이팅 모달 변경

--- devops-final-front/src/components/user/Mypage.js
+++ devops-final-front/src/components/user/Mypage.js
@@ -17,8 +17,8 @@ import Pagination from "../../components/Pagination";
 import rightImg from "../../assets/images/detail/right.png";
 import leftImg from "../../assets/images/detail/left.png";
 import locationImg from "../../assets/images/detail/location.png";
-import NicknameEditModal from "../Modal/NicknameEditModal"
-import ReviewModal from "../Modal/ReviewModal"
+import NicknameEditModal from "../Modal/NicknameEditModal";
+import ReviewModal from "../Modal/ReviewModal";
 
 const Mypage = ({ userId, onCardClick }) => {
   const [editshow, setEditshow] = useState(false);
@@ -151,7 +151,6 @@ const Mypage = ({ userId, onCardClick }) => {
       }
       const data = await response.json();
       setFavorites(data);
-      console.log("Favorites:", data);
     } catch (error) {
       console.error("Error fetching favorites:", error);
     }
@@ -203,7 +202,11 @@ const Mypage = ({ userId, onCardClick }) => {
         </div>
       );
     } else if (reservation.resStatus === "VISITED") {
-      return <button className="my-res-info-review-btn" onClick={ReviewShow}>리뷰</button>;
+      return (
+        <button className="my-res-info-review-btn" onClick={ReviewShow}>
+          리뷰
+        </button>
+      );
     } else {
       return <button className="my-res-info-res-btn">예약 취소</button>;
     }
@@ -295,6 +298,20 @@ const Mypage = ({ userId, onCardClick }) => {
     console.log(restId);
   };
 
+  function formatTimestamp(timestamp) {
+    const date = new Date(timestamp);
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // 월은 0부터 시작하므로 +1 필요
+    const day = String(date.getDate()).padStart(2, "0");
+
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  }
+
   return (
     <div className="my-container">
       <div className="profile-container">
@@ -316,7 +333,9 @@ const Mypage = ({ userId, onCardClick }) => {
                 <div className="my-info-item-content">{user?.userEmail}</div>
               </div>
               <div className="my-info-item-content-box">
-                <div className="my-info-update-btn" onClick={EditShow}>회원정보 수정</div>
+                <div className="my-info-update-btn" onClick={EditShow}>
+                  회원정보 수정
+                </div>
               </div>
             </div>
             <div className="my-res-info-box">
@@ -362,7 +381,7 @@ const Mypage = ({ userId, onCardClick }) => {
                       </div>
                       <div className="my-res-info-img-wrap">
                         <img src={calImg} className="my-res-Img" />
-                        <div>{reservation.resDatetime}</div>
+                        <div>{formatTimestamp(reservation.resDatetime)}</div>
                       </div>
                       <div className="my-res-info-img-wrap">
                         <img src={peopleImg} className="my-res-Img" />
@@ -442,6 +461,8 @@ const Mypage = ({ userId, onCardClick }) => {
                 <div>
                   {waiting.waitingStatus === "QUEUE_CANCELED" ? (
                     <div>대기를 취소했습니다.</div>
+                  ) : waiting.waitingStatus === "NOSHOW" ? (
+                    <div>입장을 하지 않았습니다.</div>
                   ) : waiting.waitingLeft !== 0 ? (
                     <div>
                       현재 예상 대기시간은{" "}
@@ -456,7 +477,20 @@ const Mypage = ({ userId, onCardClick }) => {
                 </div>
               </div>
               <div className="my-waiting-info-btn-box">
-                <div className="my-waiting-info-btn" onClick={handleCancel}>
+                <div
+                  className={`my-waiting-info-btn ${
+                    waiting.waitingStatus === "QUEUE_CANCELED" ||
+                    waiting.waitingStatus === "NOSHOW"
+                      ? "disabled"
+                      : ""
+                  }`}
+                  onClick={
+                    waiting.waitingStatus === "QUEUE_CANCELED" ||
+                    waiting.waitingStatus === "NOSHOW"
+                      ? null
+                      : handleCancel
+                  }
+                >
                   <div>취소</div>
                 </div>
               </div>
@@ -573,11 +607,15 @@ const Mypage = ({ userId, onCardClick }) => {
                 <div className="my-most-card-rest-info">
                   <div className="my-most-card-title">{rest.restName}</div>
                   <div className="my-most-card-content">
-                    <img src={phoneImg} className="my-most-card-icon" alt=""/>
+                    <img src={phoneImg} className="my-most-card-icon" alt="" />
                     <div>{rest.restPhone}</div>
                   </div>
                   <div className="my-most-card-content">
-                    <img src={locationImg} className="my-most-card-icon" alt=""/>
+                    <img
+                      src={locationImg}
+                      className="my-most-card-icon"
+                      alt=""
+                    />
                     <div>{rest.restAddress}</div>
                   </div>
                 </div>
@@ -586,14 +624,8 @@ const Mypage = ({ userId, onCardClick }) => {
           </div>
         </div>
       </div>
-      <NicknameEditModal 
-      EditClose={EditClose}
-      editshow={editshow}
-      />
-      <ReviewModal 
-      ReviewClose={ReviewClose}
-      reviewshow={reviewshow}
-      />
+      <NicknameEditModal EditClose={EditClose} editshow={editshow} />
+      <ReviewModal ReviewClose={ReviewClose} reviewshow={reviewshow} />
     </div>
   );
 };
