e14995a0bfd9dd37397861ef3e84f15f7041a535
starht <star2thb@gmail.com>
2024-07-24 17:23:06 +0900

    Feat: 사용자 닉네임 수정 API 연동

--- devops-final-front/src/components/Modal/NicknameEditModal.js
+++ devops-final-front/src/components/Modal/NicknameEditModal.js
@@ -1,14 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Logo from "../../assets/images/oriblue.png";
 import "../../css/components/Modal/SigninModal.css";
 import "../../css/components/Modal/NicknameEditModal.css";
 
-function NicknameEditModal({ EditClose, editshow }) {
+function NicknameEditModal({ EditClose, editshow, userId, onSuccess }) {
   const [nickname, setNickname] = useState("");
 
-  const handleEditActivate = () => {
+  const handleEditActivate = async (e) => {
+    e.preventDefault();
     console.log(nickname);
-    EditClose();
+
+    const payload = {
+      userNickname: nickname,
+    };
+
+    try {
+      const response = await fetch(`http://localhost:8080/user/${userId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to edit nickname");
+      }
+
+      const data = await response.json();
+      console.log("닉네임 수정 완료:", data);
+      alert("닉네임이 성공적으로 수정되었습니다.");
+      onSuccess();
+      EditClose();
+    } catch (error) {
+      console.error("닉네임 수정 실패:", error);
+      alert("닉네임 수정을 실패했습니다.");
+    }
   };
 
   return (
@@ -25,8 +51,8 @@ function NicknameEditModal({ EditClose, editshow }) {
     >
       <div className={`signinModal ${editshow ? "signinshow" : "signinhide"}`}>
         <div className="signinModalHeader">
-          <img src={Logo} alt="Logo" className="signinori"/>
-          <button className='signinclosebtn' onClick={EditClose}>
+          <img src={Logo} alt="Logo" className="signinori" />
+          <button className="signinclosebtn" onClick={EditClose}>
             X
           </button>
         </div>
@@ -34,20 +60,24 @@ function NicknameEditModal({ EditClose, editshow }) {
           <div className="signinboldText">닉네임 수정</div>
           <div className="signinhintText">수정 할 닉네임을 적어주세요</div>
         </div>
-        <div className='inputBoxinNickname'>
-          <div className='NicknameHinttest'>닉네임</div>
+        <div className="inputBoxinNickname">
+          <div className="NicknameHinttest">닉네임</div>
           <input
-            type='text'
-            className='nicknameinputBox'
-            placeholder='닉네임(최대 20자)'
+            type="text"
+            className="nicknameinputBox"
+            placeholder="닉네임(최대 20자)"
             maxLength={20}
             value={nickname}
             onChange={(e) => setNickname(e.target.value)}
           />
         </div>
         <div className="signinModalButton">
-          <button className="signinModalPersonal" onClick={handleEditActivate}>수정</button>
-          <button className="signinModalRest" onClick={EditClose}>취소</button>
+          <button className="signinModalPersonal" onClick={handleEditActivate}>
+            수정
+          </button>
+          <button className="signinModalRest" onClick={EditClose}>
+            취소
+          </button>
         </div>
       </div>
     </div>
