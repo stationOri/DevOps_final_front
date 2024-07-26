8b698c0f2d7f1fb836713e8d6eca309295422521
starht <star2thb@gmail.com>
2024-07-26 13:59:53 +0900

    Feat: 식당 opentime 수정

--- devops-final-front/src/components/user/Restaurants.js
+++ devops-final-front/src/components/user/Restaurants.js
@@ -4,13 +4,13 @@ import RestCard from "../../components/RestCard";
 import Pagination from "../../components/Pagination";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import Loading from "../../components/Loading";
 
 const Restaurants = ({ userId, onCardClick }) => {
   const [initialLoading, setInitialLoading] = useState(true);
   const [additionalLoading, setAdditionalLoading] = useState(false);
   const [error, setError] = useState(null);
   const [restaurants, setRestaurants] = useState([]);
-  const [opentimes, setOpentimes] = useState([]);
   const [favorites, setFavorites] = useState([]);
   const [keywords, setKeywords] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
@@ -20,33 +20,12 @@ const Restaurants = ({ userId, onCardClick }) => {
   const [page, setPage] = useState(1);
   const [hasMore, setHasMore] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
+  const [maxPages, setMaxPages] = useState(5);
+
   const restaurantsPerPage = 6;
 
-  const convertDayToKorean = (day) => {
-    switch (day) {
-      case "MON":
-        return "월요일";
-      case "TUE":
-        return "화요일";
-      case "WED":
-        return "수요일";
-      case "THU":
-        return "목요일";
-      case "FRI":
-        return "금요일";
-      case "SAT":
-        return "토요일";
-      case "SUN":
-        return "일요일";
-      case "HOL":
-        return "공휴일";
-      default:
-        return day;
-    }
-  };
-
   useEffect(() => {
-    fetchInitialData().finally(() => setInitialLoading(false));
+    fetchInitialData();
   }, []);
 
   useEffect(() => {
@@ -55,7 +34,7 @@ const Restaurants = ({ userId, onCardClick }) => {
         setPage((prevPage) => prevPage + 1);
         fetchRestaurants(page);
       }
-    }, 5000);
+    }, 2000);
 
     return () => clearInterval(interval);
   }, [page, hasMore]);
@@ -69,7 +48,7 @@ const Restaurants = ({ userId, onCardClick }) => {
   const fetchInitialData = async () => {
     try {
       await Promise.all([
-        fetchRestaurants(1, 10),
+        fetchRestaurants(1),
         fetchFavorites(),
         fetchKeywords(),
       ]);
@@ -89,24 +68,29 @@ const Restaurants = ({ userId, onCardClick }) => {
     }
 
     try {
-      const response = await fetch(
-        `http://localhost:8080/restaurants/page/${page}`
-      );
-      if (!response.ok) {
-        throw new Error("Failed to fetch restaurants");
-      }
-      const data = await response.json();
+      if (page <= (maxPages || Infinity)) {
+        // 최대 페이지 수를 확인
+        const response = await fetch(
+          `http://localhost:8080/restaurants/page/${page}`
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch restaurants");
+        }
+        const data = await response.json();
 
-      if (data.length === 0) {
-        setHasMore(false);
+        if (data.length === 0) {
+          setHasMore(false);
+        } else {
+          setRestaurants((prevRestaurants) => {
+            // 새로운 Set 객체를 생성하여 중복을 제거합니다
+            const uniqueRestaurants = new Set(
+              [...prevRestaurants, ...data].map(JSON.stringify)
+            );
+            return Array.from(uniqueRestaurants).map(JSON.parse);
+          });
+        }
       } else {
-        setRestaurants((prevRestaurants) => {
-          // 새로운 Set 객체를 생성하여 중복을 제거합니다
-          const uniqueRestaurants = new Set(
-            [...prevRestaurants, ...data].map(JSON.stringify)
-          );
-          return Array.from(uniqueRestaurants).map(JSON.parse);
-        });
+        setHasMore(false);
       }
     } catch (error) {
       console.error("Error fetching restaurants:", error);
@@ -133,39 +117,6 @@ const Restaurants = ({ userId, onCardClick }) => {
       console.error("Error fetching restaurants by name:", error);
     }
   };
-
-  const fetchOpentimes = useCallback(async (restId) => {
-    try {
-      const response = await fetch(`http://localhost:8080/opentime/${restId}`);
-      // if (!response.ok) {
-      //   throw new Error("Failed to fetch opentimes");
-      // }
-      const data = await response.json();
-
-      const opentimesFormatted = data.map((opentime) => ({
-        ...opentime,
-        restDay: convertDayToKorean(opentime.restDay),
-      }));
-
-      setOpentimes((prevOpentimes) => [
-        ...prevOpentimes.filter((opentime) => opentime.restId !== restId),
-        ...opentimesFormatted,
-      ]);
-    } catch (error) {
-      // console.error("Error fetching opentimes:", error);
-      setError(null);
-    }
-  }, []);
-
-  useEffect(() => {
-    restaurants.forEach((restaurant) => {
-      if (
-        !opentimes.some((opentime) => opentime.restId === restaurant.restId)
-      ) {
-        fetchOpentimes(restaurant.restId);
-      }
-    });
-  }, [restaurants, fetchOpentimes, opentimes]);
 
   useEffect(() => {
     filterRestaurants();
@@ -266,9 +217,10 @@ const Restaurants = ({ userId, onCardClick }) => {
   }, [filterRestaurants]);
 
   const extractLocations = () => {
-    const locations = restaurants.map((restaurant) =>
-      restaurant.restAddress.split(" ").slice(0, 2).join(" ")
-    );
+    const locations = restaurants.map((restaurant) => {
+      const address = restaurant.restAddress || "";
+      return address.split(" ").slice(0, 2).join(" ");
+    });
     return [...new Set(locations)];
   };
 
@@ -325,111 +277,111 @@ const Restaurants = ({ userId, onCardClick }) => {
 
   return (
     <div className="restaurants">
-      <div className="rest-search-container">
-        <div className="rest-search-name">
-          <button className="rest-search-btn" onClick={handleSearchClick}>
-            <FontAwesomeIcon icon={faSearch} />
-          </button>
-          <input
-            type="text"
-            placeholder="Search"
-            className="rest-search-input"
-            value={searchTerm}
-            onChange={handleSearchChange}
-            onKeyDown={handleSearchByEnter}
-          />
-        </div>
-        <div className="rest-search-location">
-          <select
-            className="rest-location-dropdown"
-            value={locationFilter}
-            onChange={handleLocationChange}
-          >
-            <option value="">Location</option>
-            {extractLocations().map((location, index) => (
-              <option key={index} value={location}>
-                {location}
-              </option>
-            ))}
-          </select>
-        </div>
-      </div>
-      <div className="rest-keyword-con">
-        <div className="rest-keyword-container">
-          <div className="rest-keyword-box">
-            <div className="rest-keyword-title">
-              <div className="rest-keyword-title-text">
-                <div>어떤 음식을</div>
-                <div>좋아하세요?</div>
+      {initialLoading && <Loading />}
+      {!initialLoading && (
+        <>
+          <div className="rest-search-container">
+            <div className="rest-search-name">
+              <button className="rest-search-btn" onClick={handleSearchClick}>
+                <FontAwesomeIcon icon={faSearch} />
+              </button>
+              <input
+                type="text"
+                placeholder="Search"
+                className="rest-search-input"
+                value={searchTerm}
+                onChange={handleSearchChange}
+                onKeyDown={handleSearchByEnter}
+              />
+            </div>
+            <div className="rest-search-location">
+              <select
+                className="rest-location-dropdown"
+                value={locationFilter}
+                onChange={handleLocationChange}
+              >
+                <option value="">Location</option>
+                {extractLocations().map((location, index) => (
+                  <option key={index} value={location}>
+                    {location}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <div className="rest-keyword-con">
+            <div className="rest-keyword-container">
+              <div className="rest-keyword-box">
+                <div className="rest-keyword-title">
+                  <div className="rest-keyword-title-text">
+                    <div>어떤 음식을</div>
+                    <div>좋아하세요?</div>
+                  </div>
+                </div>
+              </div>
+              <div className="rest-keyword-btn-box">
+                {keywords.map((keyword) => (
+                  <div
+                    key={keyword.id}
+                    className={`rest-keyword-btn ${
+                      keywordFilters.includes(keyword.keyword) ? "active" : ""
+                    }`}
+                    onClick={() => handleKeywordClick(keyword.keyword)}
+                  >
+                    {keyword.keyword}
+                  </div>
+                ))}
               </div>
             </div>
           </div>
-          <div className="rest-keyword-btn-box">
-            {keywords.map((keyword) => (
-              <div
-                key={keyword.id}
-                className={`rest-keyword-btn ${
-                  keywordFilters.includes(keyword.keyword) ? "active" : ""
-                }`}
-                onClick={() => handleKeywordClick(keyword.keyword)}
-              >
-                {keyword.keyword}
-              </div>
-            ))}
+          <div className="rest-list-box">
+            {filteredRestaurants.length === 0 ? (
+              <div className="rest-no-list">해당 식당이 없습니다</div>
+            ) : (
+              filteredRestaurants
+                .slice(
+                  (currentPage - 1) * restaurantsPerPage,
+                  currentPage * restaurantsPerPage
+                )
+                .map((restaurant) => {
+                  return (
+                    <div className="rest-card-li" key={restaurant.restId}>
+                      <RestCard
+                        userId={3}
+                        restId={restaurant.restId}
+                        img={restaurant.restPhoto}
+                        RestName={restaurant.restName}
+                        RestAddress={restaurant.restAddress}
+                        RestOpentimes={restaurant.restOpentimes}
+                        keyword1={restaurant.keyword1}
+                        keyword2={restaurant.keyword2}
+                        keyword3={restaurant.keyword3}
+                        isFavorite={
+                          favorites.some(
+                            (fav) =>
+                              fav.restaurantId === restaurant.restId &&
+                              fav.userId === userId
+                          )
+                            ? true
+                            : false
+                        }
+                        toggleFavorite={toggleFavorite}
+                        onCardClick={() => handleCardClick(restaurant.restId)}
+                      />
+                    </div>
+                  );
+                })
+            )}
           </div>
-        </div>
-      </div>
-      <div className="rest-list-box">
-        {filteredRestaurants.length === 0 ? (
-          <div className="rest-no-list">해당 식당이 없습니다</div>
-        ) : (
-          filteredRestaurants
-            .slice(
-              (currentPage - 1) * restaurantsPerPage,
-              currentPage * restaurantsPerPage
-            )
-            .map((restaurant) => {
-              const restaurantOpentimes = opentimes.filter(
-                (opentime) =>
-                  Number(opentime.restId) === Number(restaurant.restId)
-              );
-
-              return (
-                <div className="rest-card-li" key={restaurant.restId}>
-                  <RestCard
-                    userId={3}
-                    restId={restaurant.restId}
-                    img={restaurant.restPhoto}
-                    RestName={restaurant.restName}
-                    RestAddress={restaurant.restAddress}
-                    RestOpentimes={restaurantOpentimes}
-                    keyword1={restaurant.keyword1}
-                    keyword2={restaurant.keyword2}
-                    keyword3={restaurant.keyword3}
-                    isFavorite={
-                      favorites.some(
-                        (fav) =>
-                          fav.restaurantId === restaurant.restId &&
-                          fav.userId === userId
-                      )
-                        ? true
-                        : false
-                    }
-                    toggleFavorite={toggleFavorite}
-                    onCardClick={() => handleCardClick(restaurant.restId)}
-                  />
-                </div>
-              );
-            })
-        )}
-      </div>
-      <Pagination
-        totalItems={filteredRestaurants.length}
-        itemsPerPage={restaurantsPerPage}
-        currentPage={currentPage}
-        onPageChange={onPageChange}
-        activeColor="#FF8A00"
-      />
+          <Pagination
+            totalItems={filteredRestaurants.length}
+            itemsPerPage={restaurantsPerPage}
+            currentPage={currentPage}
+            onPageChange={onPageChange}
+            activeColor="#FF8A00"
+          />
+        </>
+      )}
     </div>
   );
 };
