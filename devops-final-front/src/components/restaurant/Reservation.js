aa14cf99ed7739f259b19dbf02241aae0717c989
01022549653@naver.com <>
2024-07-26 09:18:09 +0900

    Feat: 예약 안하는 경우 알림톡 버튼 안뜨게

--- devops-final-front/src/components/restaurant/Reservation.js
+++ devops-final-front/src/components/restaurant/Reservation.js
@@ -12,7 +12,7 @@ import Deposit from "./Deposit";
 import Today from "./Today";
 import DetailedReservation from "./DetailedReservation";
 
-function Reservation({ restId, onMenuClick, }) {
+function Reservation({ restId, onMenuClick }) {
   const addDays = (date, days) => {
     const newDate = new Date(date);
     newDate.setDate(newDate.getDate() + days);
@@ -23,9 +23,9 @@ function Reservation({ restId, onMenuClick, }) {
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [tableDates, setTableDates] = useState({
     today: selectedDate,
-      day1: addDays(selectedDate, 1),
-      day2: addDays(selectedDate, 2),
-      day3: addDays(selectedDate, 3),
+    day1: addDays(selectedDate, 1),
+    day2: addDays(selectedDate, 2),
+    day3: addDays(selectedDate, 3),
   });
   const [rev1, setRev1] = useState([]);
   const [rev2, setRev2] = useState([]);
@@ -46,13 +46,14 @@ function Reservation({ restId, onMenuClick, }) {
 
   const getWait = useCallback(async () => {
     try {
-      const response = await fetch(`http://localhost:8080/restaurants/info/revWait/${restId}`);
+      const response = await fetch(
+        `http://localhost:8080/restaurants/info/revWait/${restId}`
+      );
       if (!response.ok) {
         throw new Error("Failed to fetch");
       }
       const text = await response.text();
       setRev(text === "B" || text === "C");
-      console.log(rev+text)
     } catch (error) {
       console.error("Error revWait status:", error);
     }
@@ -66,10 +67,26 @@ function Reservation({ restId, onMenuClick, }) {
 
     try {
       const [response1, response2, response3, response4] = await Promise.all([
-        axios.get(`http://localhost:8080/reservations/reservation/rest/${restId}/${formatDateFetch(tableDates.today)}`),
-        axios.get(`http://localhost:8080/reservations/reservation/rest/${restId}/${formatDateFetch(tableDates.day1)}`),
-        axios.get(`http://localhost:8080/reservations/reservation/rest/${restId}/${formatDateFetch(tableDates.day2)}`),
-        axios.get(`http://localhost:8080/reservations/reservation/rest/${restId}/${formatDateFetch(tableDates.day3)}`),
+        axios.get(
+          `http://localhost:8080/reservations/reservation/rest/${restId}/${formatDateFetch(
+            tableDates.today
+          )}`
+        ),
+        axios.get(
+          `http://localhost:8080/reservations/reservation/rest/${restId}/${formatDateFetch(
+            tableDates.day1
+          )}`
+        ),
+        axios.get(
+          `http://localhost:8080/reservations/reservation/rest/${restId}/${formatDateFetch(
+            tableDates.day2
+          )}`
+        ),
+        axios.get(
+          `http://localhost:8080/reservations/reservation/rest/${restId}/${formatDateFetch(
+            tableDates.day3
+          )}`
+        ),
       ]);
 
       setRev1(sortByDateTime(response1.data));
@@ -81,7 +98,8 @@ function Reservation({ restId, onMenuClick, }) {
     }
   }, []);
 
-  const sortByDateTime = (data) => data.sort((a, b) => new Date(a.resDate) - new Date(b.resDate));
+  const sortByDateTime = (data) =>
+    data.sort((a, b) => new Date(a.resDate) - new Date(b.resDate));
 
   useEffect(() => {
     const fetchData = async () => {
@@ -116,8 +134,6 @@ function Reservation({ restId, onMenuClick, }) {
     return `${year}-${month}-${day}`;
   };
 
-  
-
   useEffect(() => {
     setTableDates({
       today: new Date(),
@@ -128,9 +144,9 @@ function Reservation({ restId, onMenuClick, }) {
   }, []);
 
   const handleDateChange = (date) => setSelectedDate(date);
-  const gotoRestInfo = () =>{
-     onMenuClick("계정 정보");
-  }
+  const gotoRestInfo = () => {
+    onMenuClick("계정 정보");
+  };
   return (
     <div className="WrapperWithoutBorder">
       {loading ? (
@@ -138,7 +154,7 @@ function Reservation({ restId, onMenuClick, }) {
       ) : (
         <>
           <div className="UpperBoxRestheight">
-            <Deposit restId={restId}/>
+            <Deposit restId={restId} />
             <Today rev={rev} restId={restId} />
           </div>
           <div className="LowerBoxRest">
@@ -165,10 +181,14 @@ function Reservation({ restId, onMenuClick, }) {
                 </button>
               </div>
               <div className="phoneWrapper">
+                { rev&&(
+                  <>
                 <img src={Phone} alt="" className="phoneicon" />
                 <div className="phoneText" onClick={TalkShow}>
                   알림톡 전송하기
                 </div>
+                </>
+                ) }
               </div>
             </div>
             {rev ? (
@@ -177,28 +197,44 @@ function Reservation({ restId, onMenuClick, }) {
                   <div className="tableBorderyes">
                     {formatDate(tableDates.today)}
                   </div>
-                  <DetailedReservation reservations={rev1} row={1} restId={restId} />
+                  <DetailedReservation
+                    reservations={rev1}
+                    row={1}
+                    restId={restId}
+                  />
                 </div>
                 <div className="colWarpper">
                   <div className="tableBorderyes">
                     {formatDate(tableDates.day1)} (D+1)
                   </div>
-                  <DetailedReservation reservations={rev2} row={2} restId={restId} />
+                  <DetailedReservation
+                    reservations={rev2}
+                    row={2}
+                    restId={restId}
+                  />
                 </div>
                 <div className="colWarpper">
                   <div className="tableBorderyes">
                     {formatDate(tableDates.day2)} (D+2)
                   </div>
-                  <DetailedReservation reservations={rev3} row={3} restId={restId} />
+                  <DetailedReservation
+                    reservations={rev3}
+                    row={3}
+                    restId={restId}
+                  />
                 </div>
                 <div className="colWarpper">
                   <div className="tableBorderyesend">
                     {formatDate(tableDates.day3)} (D+3)
                   </div>
-                  <DetailedReservation reservations={rev4} row={4} restId={restId} />
+                  <DetailedReservation
+                    reservations={rev4}
+                    row={4}
+                    restId={restId}
+                  />
                 </div>
               </div>
-            ):(
+            ) : (
               <div className="bookingtableno">
                 <div className="rowWrapper">
                   <div className="tableBorder forRow">
@@ -217,16 +253,23 @@ function Reservation({ restId, onMenuClick, }) {
                 <div className="rowWrapper2">
                   <div>예약 기능을 사용하지 않고 있습니다.</div>
                   <div>
-                    해당 기능을 사용하려면 가게 정보 수정 화면에서 예약을 활성화 해주세요.
+                    해당 기능을 사용하려면 가게 정보 수정 화면에서 예약을 활성화
+                    해주세요.
                   </div>
-                  <button onClick={gotoRestInfo}>가게 정보 수정하러 가기</button>
+                  <button onClick={gotoRestInfo}>
+                    가게 정보 수정하러 가기
+                  </button>
                 </div>
               </div>
             )}
           </div>
         </>
       )}
-      <SendTalkModal TalkClose={TalkClose} talkshow={talkshow} restId={restId} />
+      <SendTalkModal
+        TalkClose={TalkClose}
+        talkshow={talkshow}
+        restId={restId}
+      />
     </div>
   );
 }
